c4d56113ded06d97b51cfe05ecace0c3197b658c
sofíaN <91507303+SofiaNeira@users.noreply.github.com>
2022-05-18 09:28:00 -0500

    first commit

---
+++ src/main/java/com/co/ias/Handyman/application/person/model/PersonDTO.java
@@ -0,0 +1,91 @@
+package com.co.ias.Handyman.application.person.model;
+
+import com.co.ias.Handyman.application.person.domain.*;
+
+public class PersonDTO {
+
+    /*
+      Person DTO Attributes
+    */
+    private Integer personId;
+    private String personNames;
+    private String personIdentification;
+    private Integer idTypeIdentification;
+
+    /*
+      No Args Constructor
+    */
+    public PersonDTO() {
+    }
+
+    /*
+     All Args Constructor
+    */
+    public PersonDTO(Integer personId, String personNames, String personIdentification, Integer idTypeIdentification) {
+        this.personId = personId;
+        this.personNames = personNames;
+        this.personIdentification = personIdentification;
+        this.idTypeIdentification = idTypeIdentification;
+    }
+
+    /*
+      Method to domain
+    */
+    public Person toDomain(){
+        return new Person(
+                new PersonId(personId),
+                new PersonNames(personNames),
+                new PersonIdentification(personIdentification),
+                new IdTypeIdentification(idTypeIdentification)
+        );
+    }
+
+
+    /*
+      Method from domain
+    */
+    public static PersonDTO fromDomain(Person person){
+        return new PersonDTO(
+                person.getPersonId().getValue(),
+                person.getPersonNames().getValue(),
+                person.getPersonIdentification().getValue(),
+                person.getIdTypeIdentification().getValue()
+        );
+
+    }
+
+    /*
+      Getters and Setters
+    */
+    public Integer getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(Integer personId) {
+        this.personId = personId;
+    }
+
+    public String getPersonNames() {
+        return personNames;
+    }
+
+    public void setPersonNames(String personNames) {
+        this.personNames = personNames;
+    }
+
+    public String getPersonIdentification() {
+        return personIdentification;
+    }
+
+    public void setPersonIdentification(String personIdentification) {
+        this.personIdentification = personIdentification;
+    }
+
+    public Integer getIdTypeIdentification() {
+        return idTypeIdentification;
+    }
+
+    public void setIdTypeIdentification(Integer idTypeIdentification) {
+        this.idTypeIdentification = idTypeIdentification;
+    }
+}
